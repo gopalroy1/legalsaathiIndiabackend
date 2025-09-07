7500a7edf10cee02a10e105d4b182ffb16df896e
gopalroy1 <gopalroy5397@gmail.com>
2025-09-07 17:42:04 +0530

    added new mapping cors

--- src/main/java/com/example/demo/Config/SecurityConfig.java
+++ src/main/java/com/example/demo/Config/SecurityConfig.java
@@ -97,7 +97,7 @@ public class SecurityConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000","https://www.legalsaathiindia.com","http://127.0.0.1:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true)
                 .allowedHeaders("*");
